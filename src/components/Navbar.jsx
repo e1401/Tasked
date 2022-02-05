52843249a941d34f95d1c928fb181be92b4d0503
e1401 <hadrovic1@gmail.com>
2022-02-05 12:29:38 +0100

    Add navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,26 @@
+//styles and assets
 import './Navbar.css';
+import TaskedLogo from '../assets/tasked-logo.svg';
+
+import { Link } from 'react-router-dom';
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <nav className="navbar">
+      <ul>
+        <li className="logo">
+          <img src={TaskedLogo} alt="Tasked logo" />
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+          <Link to="/signup">Sign up</Link>
+        </li>
+        <li>
+          <button className="btn">Logout</button>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Navbar;
