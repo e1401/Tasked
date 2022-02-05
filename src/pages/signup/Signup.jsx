2e7cc1ddb320c2993626822ae0f2001ebfcd5fd7
e1401 <hadrovic1@gmail.com>
2022-02-05 14:08:38 +0100

    Add signup form

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,7 +1,48 @@
+import { useState } from 'react';
 import './Signup.css';
 
 function Signup() {
-  return <div>Signup page</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [displayName, setDisplayName] = useState('');
+  const [thumbnail, setThumbnail] = useState(null);
+
+  return (
+    <form className="auth-form">
+      <h2>Sign up</h2>
+      <label>
+        <span>E-mail</span>
+        <input type="email" required value={email} onChange={(e) => setEmail(e.target.value)} />
+      </label>
+      <label>
+        <span>Password</span>
+        <input
+          type="password"
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      <label>
+        <span>Display name</span>
+        <input
+          type="text"
+          required
+          value={displayName}
+          onChange={(e) => setDisplayName(e.target.value)}
+        />
+      </label>
+      <label>
+        <span>Upload avatar</span>
+        <input
+          type="file"
+          required
+          //upload later on
+        />
+      </label>
+      <button className="btn">Sign up</button>
+    </form>
+  );
 }
 
 export default Signup;
