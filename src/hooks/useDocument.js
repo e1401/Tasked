4923b95e00d13117035f80937b3435470e15f340
e1401 <hadrovic1@gmail.com>
2022-02-09 08:20:48 +0100

    Fetch and display a project

--- src/hooks/useDocument.js
+++ src/hooks/useDocument.js
@@ -38,8 +38,12 @@ export const useDocument = (collection, id) => {
 
     const unsubscribe = ref.onSnapshot(
       (snapshot) => {
-        setDocument({ ...snapshot.data(), id: snapshot.id });
-        setError(null);
+        if (snapshot.data()) {
+          setDocument({ ...snapshot.data(), id: snapshot.id });
+          setError(null);
+        } else {
+          setError('No such document');
+        }
       },
       (error) => {
         console.log(error.message);
--- src/pages/project/Project.jsx
+++ src/pages/project/Project.jsx
@@ -4,13 +4,21 @@ import { useParams } from 'react-router-dom';
 
 function Project() {
   const { id } = useParams();
-  const { document, error } = useDocument('projects', `${id}`);
+  const { document, error } = useDocument('projects', id);
   console.log(id);
   console.log(document, error);
+
+  if (error) {
+    return <div className="error">{error}</div>;
+  }
+
+  if (!document) {
+    return <div className="loading">Loading...</div>;
+  }
+
   return (
-    <div>
-      Project page
-      {document && <h2>{document.projectName}</h2>}
+    <div className="project-details">
+      <h1>{document.projectName}</h1>
     </div>
   );
 }
