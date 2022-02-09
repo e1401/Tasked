44c672b437684e7af66d8ca4cf5e20adf7f750e2
e1401 <hadrovic1@gmail.com>
2022-02-09 08:02:21 +0100

    Improved useDocument hook

--- src/hooks/useDocument.js
+++ src/hooks/useDocument.js
@@ -1,28 +1,56 @@
 import { projectFirestore } from '../firebase/config';
 import { useEffect, useState } from 'react';
 
+// export const useDocument = (collection, id) => {
+//   const [document, setDocument] = useState(null);
+//   const [error, setError] = useState(null);
+
+//   useEffect(() => {
+//     let ref = projectFirestore.collection(collection).doc(id);
+
+//     ref
+//       .get()
+//       .then((doc) => {
+//         if (doc.exists) {
+//           console.log(doc.data());
+//           setDocument(doc.data());
+//           setError(null);
+//         } else {
+//           console.log('No such document');
+//         }
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//       });
+//   }, [collection, id]);
+
+//   return { document, error };
+// };
+
 export const useDocument = (collection, id) => {
   const [document, setDocument] = useState(null);
   const [error, setError] = useState(null);
 
+  //realtime data for the document
   useEffect(() => {
-    let ref = projectFirestore.collection(collection).doc(id);
+    //first, get a reference to the document
+    const ref = projectFirestore.collection(collection).doc(id);
 
-    ref
-      .get()
-      .then((doc) => {
-        if (doc.exists) {
-          console.log(doc.data());
-          setDocument(doc.data());
-          setError(null);
-        } else {
-          console.log('No such document');
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    const unsubscribe = ref.onSnapshot(
+      (snapshot) => {
+        setDocument({ ...snapshot.data(), id: snapshot.id });
+        setError(null);
+      },
+      (error) => {
+        console.log(error.message);
+        setError('Unable to reach document');
+      }
+    );
+
+    //unsubscribe function from real-time listener
+    return () => {
+      unsubscribe();
+    };
   }, [collection, id]);
-
   return { document, error };
 };
